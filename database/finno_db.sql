62224adb9fe7b54595be8aa0ac836f030064bb29
JuanDi <juandixarteagaperez210@gmail.com>
2022-09-04 22:22:28 -0500

    Se agrega bd

--- database/finno_db.sql
+++ database/finno_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-07-2022 a las 12:13:17
+-- Tiempo de generación: 05-09-2022 a las 05:21:07
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -12,13 +12,153 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `finno_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `expences`
+--
+
+CREATE TABLE `expences` (
+  `id` int(11) NOT NULL,
+  `name` varchar(300) NOT NULL,
+  `amount` float NOT NULL,
+  `id_period` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `expences`
+--
+
+INSERT INTO `expences` (`id`, `name`, `amount`, `id_period`) VALUES
+(1, 'Internet', 125000, 1),
+(2, 'Servicios', 200000, 1),
+(3, 'Mercado', 100000, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `incomes`
+--
+
+CREATE TABLE `incomes` (
+  `id` int(11) NOT NULL,
+  `name` varchar(300) NOT NULL,
+  `amount` float NOT NULL,
+  `id_period` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `incomes`
+--
+
+INSERT INTO `incomes` (`id`, `name`, `amount`, `id_period`) VALUES
+(1, 'Pago de nómina Ceiba', 500000, 1),
+(2, 'Trabajo independiente', 300000, 1),
+(9, 'Pago de nómina ceiba', 500000, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `period`
+--
+
+CREATE TABLE `period` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `date_start` date NOT NULL,
+  `date_finish` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `period`
+--
+
+INSERT INTO `period` (`id`, `name`, `id_user`, `date_start`, `date_finish`) VALUES
+(1, 'Agosto - quincena 1', 111, '2022-09-01', '2022-09-15'),
+(3, 'Agosto - quincena 2', 111, '2022-09-16', '2022-09-30');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL PRIMARY KEY,
-  `name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
+  `id` int(11) NOT NULL,
+  `names` varchar(100) NOT NULL,
+  `last_names` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `adress` varchar(100) NOT NULL,
-  `phone` varchar(15) NOT NULL
+  `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `names`, `last_names`, `email`, `password`) VALUES
+(111, 'Test', 'Test', 'email@email.com', '$2b$10$g2VhO2xYVhuQPIc8IPFxB.IV5g03uO7LBClKUzuvWQfLLCbTywquC');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `expences`
+--
+ALTER TABLE `expences`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `incomes`
+--
+ALTER TABLE `incomes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `period`
+--
+ALTER TABLE `period`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `expences`
+--
+ALTER TABLE `expences`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `incomes`
+--
+ALTER TABLE `incomes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `period`
+--
+ALTER TABLE `period`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
